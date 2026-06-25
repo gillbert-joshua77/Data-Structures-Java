5e9d7d71aacd550fb2b654970a7a5a152b596f3d
gillbert-joshua77 <gillbertjoshua77@gmail.com>
2026-06-25 16:06:33 +0530

    Linked List : Insert Beginning and Display

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -1,5 +1,33 @@
+package linkedlist;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next= null;
+    }
+}
+
 class LinkedList{
+    Node head =null;
+    void insertBeginning(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+    void Display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
     public static void main(String [] args){
-        System.out.print("Hello This is Joshau");
+        LinkedList list = new LinkedList();
+        list.insertBeginning(1);
+        list.insertBeginning(2);
     }
 }
